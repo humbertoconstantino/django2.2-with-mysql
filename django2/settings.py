61535b80c2cb3bb62bb32b3fa80524c076033fa0
humbertoconstantino <humbertoconstantino18@gmail.com>
2021-04-11 12:10:38 -0300

    Forms-email

--- django2/settings.py
+++ django2/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'statifiles')
 
 LOGOUT_REDIRECT_URL = 'index'
 
-# Configrações de e-mail
+# Configurações de e-mail
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 """
 EMAIL_HOST = 'localhost'
